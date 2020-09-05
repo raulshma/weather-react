57168e7386ea7c89ed1ba763395d349e63fa14ad
raulshma <raulshma@gmail.com>
2020-09-05 12:58:17 +0000

    Deploying to gh-pages from  @ b66a4f13c26bdd682d8bfff124a38fc70ba6b6c4 🚀

---
+++ precache-manifest.39b4686f23b607936f4f502933efebb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e015632c3fcadf8decb518d250ea6884",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "0cbf107cbb743940891a",
+    "url": "/weather-react/static/css/main.6f865e57.chunk.css"
+  },
+  {
+    "revision": "ca53ce6f8b3c0326c006",
+    "url": "/weather-react/static/js/2.e3d91a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.e3d91a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbf107cbb743940891a",
+    "url": "/weather-react/static/js/main.408af63d.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-react/precache-manifest.ae73f6c2c53a7a30a96445092f110854.js"
+  "/weather-react/precache-manifest.39b4686f23b607936f4f502933efebb1.js"
 );
 
 self.addEventListener('message', (event) => {
